a2f8bd918da7c7659f7e8de11f78aea78f70ad77
EnricoPriarone <62288824+EnricoPriarone@users.noreply.github.com>
2020-04-15 12:57:39 +0200

    Update R_code_teleril.r

--- R_code_teleril.r
+++ R_code_teleril.r
@@ -56,16 +56,20 @@ plot(p224r63_2011$B4_sre, col=clnir)
 par(mfrow=c(2,2))
 
 # Inizio col blu, poi verde ecc.
-clb <- colorRampPalette(c('dark blue','blue','light blue'))(100) # blue
+# blue
+clb <- colorRampPalette(c('dark blue','blue','light blue'))(100)
 plot(p224r63_2011$B1_sre, col=clb)
 
-clg <- colorRampPalette(c('dark green','green','light green'))(100) # green
+# green
+clg <- colorRampPalette(c('dark green','green','light green'))(100)
 plot(p224r63_2011$B2_sre, col=clg) # è intermedio tra verde e NIR
 
-clr <- colorRampPalette(c('dark red','red','orange'))(100) # red
+# red
+clr <- colorRampPalette(c('dark red','red','orange'))(100)
 plot(p224r63_2011$B3_sre, col=clr) # Sono bassi perché quasi tutto rosso è assorbito per fotosintesi
 
-clnir <- colorRampPalette(c('red','orange','yellow'))(100) # NIR
+# NIR
+clnir <- colorRampPalette(c('red','orange','yellow'))(100)
 plot(p224r63_2011$B4_sre, col=clnir) # Valori di NIR sono molto alti -> buona parte è foresta
 
 # Montiamo le bande insieme per fare immagine come la vedrebbe l'occhio umano (con funz. "plotRGB")
@@ -103,3 +107,137 @@ plotRGB(p224r63_2011, r=3, g=4, b=2, stretch="Lin")
 # Ora lo metto nel blu
 plotRGB(p224r63_2011, r=3, g=2, b=4, stretch="Lin")
 dev.off()
+
+# 15/04
+# Richiamo le librerie, la working directory e il file .RData
+library(raster)
+setwd("/Users/enricopriarone/lab")
+load(".RData")
+ls()
+
+# Importo file del 1988 con tutte le bande
+# "grd" sta per griglia: formato è di righe e colonne
+p224r63_1988 <- brick("p224r63_1988_masked.grd")
+
+# Facciamo un plot dell'immagine
+plot(p224r63_1988)
+
+# Rifacciamo il lavoro dell'altra volta con l0immagine del 1988
+par(mfrow=c(2,2))
+
+# Inizio col blu, poi verde ecc.
+# blue
+clb <- colorRampPalette(c('dark blue','blue','light blue'))(100)
+plot(p224r63_1988$B1_sre, col=clb)
+
+# green
+clg <- colorRampPalette(c('dark green','green','light green'))(100)
+plot(p224r63_1988$B2_sre, col=clg) # è intermedio tra verde e NIR
+
+# red
+clr <- colorRampPalette(c('dark red','red','orange'))(100)
+plot(p224r63_1988$B3_sre, col=clr) # Sono bassi perché quasi tutto rosso è assorbito per fotosintesi
+
+# NIR
+clnir <- colorRampPalette(c('red','orange','yellow'))(100)
+plot(p224r63_1988$B4_sre, col=clnir) # Valori di NIR sono molto alti -> buona parte è foresta
+
+# Faccio plot con le tre bande principali
+# B1: blu;
+# B2: verde;
+# B3: rosso;
+# B4: NIR;
+# B5: infr. medio; 
+# B6: infr. termico;
+# B7: infr. medio.
+plotRGB(p224r63_1988, r=3, g=2, b=1, stretch="Lin")
+
+# Esercizio: fare plot dell'immagine usando il NIR nella componente "r"
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin") # Già che ci sono sposto tutte le bande di 1: da 4-2-1 a 4-3-2
+
+# Faccio plot delle due immagini (1988 e 2011)
+# Devo fare un multipanel: funz. "par", con "mfrow"
+par(mfrow=c(2,1))
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+# Confronto: agricolo è molto più sviluppato nel 2011
+
+dev.off()
+
+# Creo un indice per verificare le differenze
+# DVI=NIR-RED, e questo si fa pixel per pixel
+# Indice spettrale DVI
+# dvi_1988 = nir1988-red1988
+# uso il $ per collegare le bande al dataset
+dvi_1988 <- p224r63_1988$B4_sre - p224r63_1988$B3_sre
+plot(dvi_1988)
+
+# Esercizio: calcolare DVI del 2011
+dvi_2011 <- p224r63_2011$B4_sre - p224r63_2011$B3_sre
+plot(dvi_2011)
+
+# Provo altri colori
+cldvi <- colorRampPalette(c('light blue','light green','green'))(100)
+plot(dvi_2011, col=cldvi)
+
+# Faccio differenza nel tempo tra i due indici: multitemporal analysis
+# Valore è positivo se vegetazione è in buone condizioni nel 2011 rispetto al 1988
+# Viceversa, se è negativo ora sta peggio
+difdvi <- dvi_2011 - dvi_1988
+plot(difdvi)
+
+# Cambio i colori:
+cldifdvi <- colorRampPalette(c('red','white','blue'))(100)
+plot(difdvi, col=cldifdvi)
+
+# Faccio par multiframe per vedere le tre immagini
+par(mfrow=c(3,1))
+plotRGB(p224r63_1988, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plot(difdvi, col=cldifdvi)
+dev.off()
+
+# Provo a cambiare la risoluzione delle immagini
+# con pixel più grandi, dunque a risoluzione minore
+# La riduco di n volte: se pixel è di 30 m, fact=10 mi dà nuovo pixel di 300 m
+p224r63_2011lr <- aggregate(p224r63_2011, fact=10)
+
+# Guardo caratteristiche immagini
+# imm. originale ha resolution 30x30
+# nuova 300x300 e righe sono meno
+p224r63_2011
+p224r63_2011lr
+
+# Faccio grafico
+par(mfrow=c(2,1))
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011lr, r=4, g=3, b=2, stretch="Lin")
+
+# Riduco ancora
+p224r63_2011lr50 <- aggregate(p224r63_2011, fact=50)
+p224r63_2011lr50
+
+par(mfrow=c(3,1))
+plotRGB(p224r63_2011, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011lr, r=4, g=3, b=2, stretch="Lin")
+plotRGB(p224r63_2011lr50, r=4, g=3, b=2, stretch="Lin")
+dev.off()
+
+# Faccio dvi a bassa risoluzione
+dvi_2011lr50 <- p224r63_2011lr50$B4_sre - p224r63_2011lr50$B3_sre
+plot(dvi_2011lr50) # Ma con questo perdo tantissimo e sembra tutto omogeneo
+
+# Faccio lo stesso per il 1988
+p224r63_1988lr50 <- aggregate(p224r63_1988, fact=50)
+dvi_1988lr50 <- p224r63_1988lr50$B4_sre - p224r63_1988lr50$B3_sre
+
+# Faccio difdvi a bassa risoluzione
+difdvilr50 <- dvi_2011lr50 - dvi_1988lr50
+cldifdvi <- colorRampPalette(c('red','white','blue'))(100)
+plot(difdvilr50,col=cldifdvi)
+
+# multiframe
+par(mfrow=c(2,1))
+plot(difdvi, col=cldifdvi)
+plot(difdvilr50, col=cldifdvi)
+# Noto dalla differenza che nella prima immagine posso individuare le micro disomogeneità
